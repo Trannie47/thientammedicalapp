e8920cbe9dee228fec42cc5b179ff8b70863eb99
lecongtoai0901 <lecongtoai036953@gmail.com>
2025-11-19 20:40:30 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'pages/splash/page.dart';
+import 'pages/login/page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'THIÊN TÂM',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+      routes: {
+        '/splash': (context) => const SplashPage(),
+        '/login': (context) => const LoginPage(),
+      },
+    );
+  }
+}
+
